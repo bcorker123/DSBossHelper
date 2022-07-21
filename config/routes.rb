cdafc9e97c3ae575424e5071d0df5f67cfd2e179
brian corker <bcorker123@gmail.com>
2022-07-21 17:41:38 -0400

    uncommented lines in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
   #resources :users
-  #namespace :api do 
-  resources :bosses
-  #end
+  namespace :api do 
+    resources :bosses
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  #get "*path", to: "fallback#index", constraints: ->(req) {!req.xhr? && req.format.html? }
+  get "*path", to: "fallback#index", constraints: ->(req) {!req.xhr? && req.format.html? }
 end
